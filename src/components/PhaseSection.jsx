a015bf4bc5a6de2daca0ae9a90cc4319334bec25
lancastre22 <manulancastre@gmail.com>
2026-05-27 11:42:52 -0400

    feat: add PhaseSection component

---
+++ src/components/PhaseSection.jsx
@@ -0,0 +1,21 @@
+import DayCard from './DayCard'
+
+export default function PhaseSection({ phase }) {
+  return (
+    <section className="phase">
+      <div className="phase__header">
+        <span className="phase__icon">{phase.emoji}</span>
+        <div>
+          <h2 className="phase__name">{phase.name}</h2>
+          <p className="phase__dates">{phase.dates}</p>
+        </div>
+      </div>
+      <div className={`phase__line phase__line--${phase.color}`} />
+      <div className="phase__days">
+        {phase.days.map((day) => (
+          <DayCard key={day.date} day={day} color={phase.color} />
+        ))}
+      </div>
+    </section>
+  )
+}
